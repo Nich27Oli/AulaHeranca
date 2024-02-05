3696763533f485e5244ec6940898ea5d463df86f
Nicholas Oliveira <nich27oli@gmail.com>
2024-02-05 12:55:04 -0300

    Heranca1

---
+++ Heranca1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Heranca1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BusinessAccount account = new BusinessAccount(838, "Nicholas", 30, 100);
+
+            
+        }
+    }
+}
